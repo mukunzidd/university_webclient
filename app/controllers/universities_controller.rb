dfe4bbc025945929674dcbb90ff35f090cc40829
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-02-16 14:26:50 +0200

    added universitites controller and RESTful actions.

---
+++ app/controllers/universities_controller.rb
@@ -0,0 +1,32 @@
+class UniversitiesController < ApplicationController
+	before_action :find_university, only: [:show, :edit, :update, :destroy]
+	def index
+		
+	end
+	def show
+		
+	end
+	def create
+		
+	end
+	def new
+		
+	end
+	def update
+		
+	end
+	def edit
+		
+	end
+	def destroy
+		
+	end
+
+	private
+		def find_university
+			
+		end
+		def university_params
+			
+		end
+end
