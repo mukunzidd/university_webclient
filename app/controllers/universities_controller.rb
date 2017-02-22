6c716e2e0d503756bc983b7b24df1ce6eacfead1
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-02-22 09:39:55 +0200

    Added the index action and view back in after debugging.

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,6 +1,25 @@
 class UniversitiesController < ApplicationController
+
+	def index
+		@university = University.all
+	end
+
 	def show
 		@university = University.find(params[:id])
 	end
-	
+	def new
+		
+	end
+	def create
+		
+	end
+	def edit
+		
+	end
+	def update
+		
+	end
+	def destroy
+		
+	end
 end
